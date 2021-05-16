1afa5610b0da00cb6b55eff8f76c16663b7d1eb0
Hilary Richardson <hilary.richardson@gmail.com>
2021-05-16 13:49:30 -0700

    updates to Plant model

--- server/models/Plant.js
+++ server/models/Plant.js
@@ -1,41 +1,3 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-
-class Plant extends Model { }
-
-Plant.init(
-    {
-        // define an id column
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        // image: {
-
-        // },
-        type: {
-
-        },
-        season: {
-
-        },
-        maintenance: {
-
-        },
-        waterneeds: {
-
-        }
-    },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'plant'
-    }
-);
-
 const { Schema, model } = require('mongoose');
 
 const plantSchema = new Schema(
@@ -74,5 +36,7 @@ const plantSchema = new Schema(
 
     }
 )
+
+const Plant = model('Plant', plantSchema);
 
 module.exports = Plant;
