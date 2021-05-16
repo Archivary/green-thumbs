605f52db55b5f9cf282cffaa5c3d50d553bd5321
Hilary Richardson <hilary.richardson@gmail.com>
2021-05-16 13:48:55 -0700

    updating User model

--- server/models/Plant.js
+++ server/models/Plant.js
@@ -36,4 +36,43 @@ Plant.init(
     }
 );
 
+const { Schema, model } = require('mongoose');
+
+const plantSchema = new Schema(
+    {
+        plantname: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        planttype: {
+            type: String,
+            required: true,
+            unique: false,
+            trim: true
+        },
+        season: {
+            // which type to choose for seasons?
+            type: String,
+            required: true,
+            unique: false,
+            trim: true,
+        },
+        highmaintenance: {
+            // which type to choose for low or high maintenance?
+            type: Boolean,
+            required: true,
+        },
+        waterneeds: {
+            // which type to choose for water needs?
+            type: String,
+            required: true,
+            unique: false,
+            trim: true
+        }
+
+    }
+)
+
 module.exports = Plant;
